ab6bc0547a44ab72e5d34f28cc840acd4a674040
Dylan Nagel <dylan_nagel@comcast.com>
2019-12-21 00:01:46 -0500

    exact lookup

--- main.go
+++ main.go
@@ -39,15 +39,22 @@ type Character struct {
 	SkillTypeIconUrl string
 	SpriteURL        string
 	GifURL           string
-	Nicknames		string
+	Nicknames        string
 }
+
+type exactCharacter struct {
+	Main Character   `json:"unit"`
+	Alts []Character `json:"versions"`
+}
+
 var db_url string
+
 func main() {
 	db_url = os.Getenv("DATABASE_URL")
-
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/lookup", lookup).Methods("GET", "OPTIONS")
-	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
+	router.HandleFunc("/lookup1.1", lookup2).Methods("GET", "OPTIONS")
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
 
 func lookup(w http.ResponseWriter, r *http.Request) {
@@ -58,20 +65,84 @@ func lookup(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("name:" + name)
 
 	if ok {
-		db, err := gorm.Open("postgres",db_url)
+		db, err := gorm.Open("postgres", db_url)
 		if err != nil {
 			fmt.Println("failed")
 
 			log.Fatalf("Unable to read client secret file: %v", err)
 		}
 		fmt.Println("connected")
-		db.Where( "'" + name + "'" +  " = ANY(characters.nicknames)").Or("en_name LIKE ?", "%" + name + "%").Or("jp_name LIKE ?", "%" + name + "%").Find(&char)
+		db.Where("'"+name+"'"+" = ANY(characters.nicknames)").Or("en_name LIKE ?", "%"+name+"%").Or("jp_name LIKE ?", "%"+name+"%").Find(&char)
 		defer db.Close()
 	}
-	for i, _ := range char {
+	for i := range char {
 		char[i].Nicknames = strings.ReplaceAll(char[i].Nicknames, "{", "[")
 		char[i].Nicknames = strings.ReplaceAll(char[i].Nicknames, "}", "]")
 	}
 	enc := json.NewEncoder(w)
 	enc.Encode(char)
+}
+
+func lookup2(w http.ResponseWriter, r *http.Request) {
+	vals := r.URL.Query()
+	names, ok := vals["name"]
+	name := names[0]
+	fmt.Println("name:" + name)
+	var char []Character
+	var exactMatch exactCharacter
+	exact := true
+	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	// 	"password=%s dbname=%s sslmode=%s",
+	// 	host, port, user, password, dbname, sslmode)
+	if ok {
+		db, err := gorm.Open("postgres", db_url)
+		db.LogMode(true)
+
+		if err != nil {
+			fmt.Println("failed")
+
+			log.Fatalf("Unable to read client secret file: %v", err)
+		}
+		fmt.Println("connected")
+
+		query := `select c.*
+from characters c
+         LEFT JOIN character_versions cv
+                   ON c.id = cv.character_id
+where (cv.version_id in
+       (select version_id
+        from character_versions cv
+                 LEFT JOIN characters c ON c.id = cv.character_id
+        where en_name = '` + name + `'
+           or '` + name + `' = ANY (c.nicknames)
+       ));`
+
+		db.Raw(query).Scan(&char)
+
+		fmt.Println(len(char))
+		if len(char) == 0 {
+			exact = false
+			db.Where("'"+name+"'"+" = ANY(characters.nicknames)").Or("en_name LIKE ?", "%"+name+"%").Or("jp_name LIKE ?", "%"+name+"%").Find(&char)
+		}
+
+		defer db.Close()
+	}
+
+	for i, _ := range char {
+		char[i].Nicknames = strings.ReplaceAll(char[i].Nicknames, "{", "[")
+		char[i].Nicknames = strings.ReplaceAll(char[i].Nicknames, "}", "]")
+	}
+	if exact {
+		for i, _ := range char {
+			if strings.ToLower(char[i].EnName) == name {
+				exactMatch.Main = char[i]
+			}
+			exactMatch.Alts = append(exactMatch.Alts, char[i])
+		}
+		enc := json.NewEncoder(w)
+		enc.Encode(exactMatch)
+	} else {
+		enc := json.NewEncoder(w)
+		enc.Encode(char)
+	}
 }
